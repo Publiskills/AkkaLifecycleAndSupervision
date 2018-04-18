b02a71c905252dc9ac9568fd8c046e238a11d577
FabienW <fabien.wirig@gmail.com>
2018-04-18 22:41:19 +0200

    Init Akka.Net project

---
+++ AkkaLifecycleAndSupervision/Program.Configuration.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using Akka.Configuration;
+
+namespace AkkaLifecycleAndSupervision
+{
+    internal partial class Program
+    {
+        private static Config GetAkkaConfigurationFromHoconFile(string configFileName = "akka.hocon")
+        {
+            return ConfigurationFactory.ParseString(File.ReadAllText(configFileName));
+        }
+    }
+}
---
+++ AkkaLifecycleAndSupervision/Program.cs
@@ -0,0 +1,14 @@
+﻿using Akka.Actor;
+
+namespace AkkaLifecycleAndSupervision
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var actorSystem = ActorSystem.Create("AkkaLifecycleAndSupervisionActorSystem", Program.GetAkkaConfigurationFromHoconFile());
+            
+            actorSystem.WhenTerminated.Wait();
+        }
+    }
+}
